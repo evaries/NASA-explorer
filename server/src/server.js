705c9d3647569d970bc5c26a1068745e3e18c32b
y.nadtochii <y.nadtochii@kuna.family>
2022-12-22 11:21:22 +0100

    added planets data

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
+const { loadPlanets } = require("./model/planets.model");
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => `Server is running on port ${PORT}`);
+async function startServer() {
+  await loadPlanets();
+  server.listen(PORT, () => `Server is running on port ${PORT}`);
+}
+
+startServer();
