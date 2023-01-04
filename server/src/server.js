f5488f7a134c2be2598410d2801adfb7fac899a4
y.nadtochii <y.nadtochii@kuna.family>
2023-01-04 08:13:17 +0100

    added mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+const MONGO_URL = require("../config");
 
 const { loadPlanets } = require("./models/planets.model");
 
@@ -8,7 +10,16 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanets();
   server.listen(PORT, () => `Server is running on port ${PORT}`);
 }
